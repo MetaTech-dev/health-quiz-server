da0ed9de42fa991785cddee83f897b8fd3a2f4dc
Matthew J Whitney <matthewjwhitney@gmail.com>
2021-01-05 16:52:21 -0700

    add process.env.port to server.listen for heroku deploy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import schema from "./schema";
 
 const server = new ApolloServer({ ...schema });
 
-server.listen().then(({ url }) => {
+server.listen(process.env.PORT || 4000).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
