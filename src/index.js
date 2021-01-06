4bddd2770e94cf2047bc254860d786ab75d1d922
Matthew J Whitney <matthewjwhitney@gmail.com>
2021-01-05 17:01:01 -0700

    enable introspection and playground for production deployment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
-const server = new ApolloServer({ ...schema });
+const server = new ApolloServer({
+  ...schema,
+  introspection: true,
+  playground: true,
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
