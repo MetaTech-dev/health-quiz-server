43d5052f4d8835ca77061e80578bc66949b59bd9
Matthew J Whitney <matthewjwhitney@gmail.com>
2021-01-09 11:41:11 -0700

    feat(Person): create, update, and delete mutations

--- src/Person/resolvers.js
+++ src/Person/resolvers.js
@@ -1,4 +1,6 @@
-const personsData = [
+import { v4 as uuid } from "uuid";
+
+let personsData = [
   {
     id: "23048073-d8e0-4b40-bb6a-72c5a1198f0e",
     firstName: "Austen",
@@ -20,6 +22,33 @@ const resolvers = {
   Query: {
     persons: () => personsData,
     person: (_parent, { id }) => personsData.find((person) => person.id === id),
+  },
+  Mutation: {
+    createPerson: (_parent, { firstName, lastName }) => {
+      const newPerson = {
+        id: uuid(),
+        firstName,
+        lastName,
+      };
+      personsData.push(newPerson);
+      console.log(personsData);
+      return newPerson;
+    },
+    updatePerson: (_parent, { id, firstName, lastName }) => {
+      const personIndex = personsData.findIndex((person) => person.id === id);
+      if (firstName) {
+        personsData[personIndex].firstName = firstName;
+      }
+      if (lastName) {
+        personsData[personIndex].lastName = lastName;
+      }
+      return personsData[personIndex];
+    },
+    deletePerson: (_parent, { id }) => {
+      personsData = personsData.filter((person) => person.id !== id);
+      console.log(personsData);
+      return "Success";
+    },
   },
 };
 
--- src/Person/typeDefs.js
+++ src/Person/typeDefs.js
@@ -3,13 +3,19 @@ import { gql } from "apollo-server";
 const typeDefs = gql`
   type Person {
     id: ID!
-    firstName: String
-    lastName: String
+    firstName: String!
+    lastName: String!
   }
 
   extend type Query {
     persons: [Person]
     person(id: ID!): Person
+  }
+
+  type Mutation {
+    createPerson(firstName: String!, lastName: String!): Person
+    updatePerson(id: ID!, firstName: String, lastName: String): Person
+    deletePerson(id: ID!): String
   }
 `;
 
